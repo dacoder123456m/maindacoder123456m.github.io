f92a8b2cf58b4941638c78fe669d3b75673fbdf2
Gd2010 <113855002+Dacoder12@users.noreply.github.com>
2023-03-18 21:18:18 -0500

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,16 @@
+const keySequence = [
+    'a',
+    'l',
+    'e',
+    'r',
+    't',
+];
+let userInput = new Array( keySequence.length );
+
+window.addEventListener( 'keydown', ( { key } ) => {
+    userInput = [ ...userInput.slice( 1 ), key ];
+
+    if ( keySequence.every( ( v, k ) => v === userInput[ k ] ) ) {
+        window.location.replace("https://classroom.google.com/");;
+    }
+} );
